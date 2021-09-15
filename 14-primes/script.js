1f189927fd0ad5d1669662c5a44ef4396ee8c226
Alexander Vaino <ale5713@ya.ru>
2021-09-15 18:41:48 +0300

    Решето Эратосфена

--- 14-primes/script.js
+++ 14-primes/script.js
@@ -6,7 +6,9 @@
  *
  */
 
-function isPrimes(num) {
+// 1 метод "Перебор"
+
+/* function isPrimes(num) {
     for (let i = 2, max = Math.sqrt(num); i <= max; i++) {
         if (num % i === 0) {
             return false;
@@ -23,6 +25,22 @@ function primes(num) {
         }
     }
     return primes;
+} */
+
+// 2 метод "Решето Эратосфена"
+
+function primes(num) {
+    const silve = [];
+    const getPrimes = [];
+    for (let i = 2; i <= num; i++) {
+        if (!silve[i]) {
+            getPrimes.push(i);
+            for (j = i * 2; j <= num; j += i) {
+                silve[j] = true;
+            }
+        }
+    }
+    return getPrimes;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
